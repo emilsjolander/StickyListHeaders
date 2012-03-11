453c6baa76aa06f43f2eea3cb4968bcd711742c7
Emil Sjölander <sjolander.emil@gmail.com>
2012-03-11 22:12:39 +0100

    added license text

--- src/com/emilsjolander/components/StickyListHeaders/StickyListHeadersAdapter.java
+++ src/com/emilsjolander/components/StickyListHeaders/StickyListHeadersAdapter.java
@@ -8,6 +8,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+/**
+ * 
+ * @author Emil Sjšlander
+ * 
+ * 
+Copyright 2012 Emil Sjšlander
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ *
+ */
 public abstract class StickyListHeadersAdapter extends BaseAdapter {
 	
 	private ArrayList<View> headerCache;
--- src/com/emilsjolander/components/StickyListHeaders/StickyListHeadersListViewWrapper.java
+++ src/com/emilsjolander/components/StickyListHeaders/StickyListHeadersListViewWrapper.java
@@ -10,7 +10,26 @@ import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
 import android.widget.FrameLayout;
 import android.widget.ListView;
+/**
+ * 
+ * @author Emil Sjšlander
+ * 
+ * 
+Copyright 2012 Emil Sjšlander
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ *
+ */
 public class StickyListHeadersListViewWrapper extends FrameLayout implements OnScrollListener {
 	
 	private static final String HEADER_HEIGHT = "headerHeight";
