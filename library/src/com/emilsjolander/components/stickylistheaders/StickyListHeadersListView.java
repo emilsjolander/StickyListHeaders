1d948c1060ea1fe5b3f220b45169e62da380d375
Emil Sjölander <sjolander.emil@gmail.com>
2013-01-31 23:36:06 +0100

    Fixes issue #63, #64

--- library/src/com/emilsjolander/components/stickylistheaders/StickyListHeadersListView.java
+++ library/src/com/emilsjolander/components/stickylistheaders/StickyListHeadersListView.java
@@ -106,6 +106,14 @@ public class StickyListHeadersListView extends ListView implements
 		setVerticalFadingEdgeEnabled(false);
 		viewConfig = ViewConfiguration.get(context);
 	}
+	
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		super.onLayout(changed, l, t, r, b);
+		if(changed){
+			reset();
+		}
+	}
 
 	private void reset() {
 		headerBottomPosition = 0;
--- sample/src/com/emilsjolander/components/stickylistheaders/test/TestBaseAdapter.java
+++ sample/src/com/emilsjolander/components/stickylistheaders/test/TestBaseAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+
 import com.emilsjolander.components.stickylistheaders.StickyListHeadersAdapter;
 
 
@@ -73,12 +74,12 @@ public class TestBaseAdapter extends BaseAdapter implements StickyListHeadersAda
 		if (convertView == null) {
 			holder = new HeaderViewHolder();
 			convertView = inflater.inflate(R.layout.header, parent, false);
-			holder.text = (TextView) convertView.findViewById(R.id.text);
+			holder.text1 = (TextView) convertView.findViewById(R.id.text1);
+			holder.text2 = (TextView) convertView.findViewById(R.id.text2);
 			convertView.setTag(holder);
 		} else {
 			holder = (HeaderViewHolder) convertView.getTag();
 		}
-
 		//set header text as first char in name
 		char headerChar = countries[position].subSequence(0, 1).charAt(0);
 		String headerText;
@@ -87,8 +88,8 @@ public class TestBaseAdapter extends BaseAdapter implements StickyListHeadersAda
 		}else{
 			headerText = headerChar + "\n" + headerChar;
 		}
-		holder.text.setText(headerText);
-
+		holder.text1.setText(headerText);
+		holder.text2.setText(headerText);
 		return convertView;
 	}
 
@@ -100,7 +101,8 @@ public class TestBaseAdapter extends BaseAdapter implements StickyListHeadersAda
 	}
 
 	class HeaderViewHolder {
-		TextView text;
+		TextView text1;
+		TextView text2;
 	}
 
 	class ViewHolder {
