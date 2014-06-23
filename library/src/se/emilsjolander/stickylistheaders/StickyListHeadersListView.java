05630d477f0afe128eef0a65f0391d6da4469cde
Eng-Fouad <engineer.fouad@hotmail.com>
2014-06-23 15:28:50 +0300

    addFooterView(View v, Object data, boolean isSelectable) is missing

--- library/src/se/emilsjolander/stickylistheaders/StickyListHeadersListView.java
+++ library/src/se/emilsjolander/stickylistheaders/StickyListHeadersListView.java
@@ -736,6 +736,10 @@ public class StickyListHeadersListView extends FrameLayout {
     public int getHeaderViewsCount() {
         return mList.getHeaderViewsCount();
     }
+    
+    public void addFooterView(View v, Object data, boolean isSelectable) {
+        mList.addFooterView(v, data, isSelectable);
+    }
 
     public void addFooterView(View v) {
         mList.addFooterView(v);
