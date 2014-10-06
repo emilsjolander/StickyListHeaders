6913cf66c5c9bf4aaf3624985fc4a4b1d4b3fb56
Martin Guillon <martin.guillon@akylas.fr>
2014-10-06 10:10:39 +0200

    allow null item, Useful to show empty section but still show the header

--- library/src/se/emilsjolander/stickylistheaders/WrapperView.java
+++ library/src/se/emilsjolander/stickylistheaders/WrapperView.java
@@ -39,23 +39,22 @@ public class WrapperView extends ViewGroup {
 	}
 
 	void update(View item, View header, Drawable divider, int dividerHeight) {
-		
-		//every wrapperview must have a list item
-		if (item == null) {
-			throw new NullPointerException("List view item must not be null.");
-		}
+
+        //a wrapperview item can be null. Useful to show empty section but still show the header
 
 		//only remove the current item if it is not the same as the new item. this can happen if wrapping a recycled view
 		if (this.mItem != item) {
 			removeView(this.mItem);
 			this.mItem = item;
-			final ViewParent parent = item.getParent();
-			if(parent != null && parent != this) {
-				if(parent instanceof ViewGroup) {
-					((ViewGroup) parent).removeView(item);
-				}
+			if (item != null) {
+    			final ViewParent parent = item.getParent();
+    			if(parent != null && parent != this) {
+    				if(parent instanceof ViewGroup) {
+    					((ViewGroup) parent).removeView(item);
+    				}
+    			}
+    			addView(item);
 			}
-			addView(item);
 		}
 
 		//same logik as above but for the header
@@ -64,22 +63,18 @@ public class WrapperView extends ViewGroup {
 				removeView(this.mHeader);
 			}
 			this.mHeader = header;
-			if (header != null) {
-                final ViewParent parent = header.getParent();
-                if(parent != null && parent != this) {
-                    if(parent instanceof ViewGroup) {
-                        ((ViewGroup) parent).removeView(header);
-                    }
-                }
-				addView(header);
-			}
-		} else if (this.mHeader != null) {
-            final ViewParent parent = this.mHeader.getParent();
+		}
+		
+		if (mHeader != null) {
+            final ViewParent parent = mHeader.getParent();
             if(parent != this) {
                 if(parent instanceof ViewGroup) {
-                    ((ViewGroup) parent).removeView(this.mHeader);
+                    ((ViewGroup) parent).removeView(mHeader);
                 }
-                addView(this.mHeader);
+                addView(mHeader);
+            }
+            if (mHeader.getVisibility() != View.VISIBLE) {
+                mHeader.setVisibility(View.VISIBLE);
             }
         }
 
@@ -108,27 +103,28 @@ public class WrapperView extends ViewGroup {
 						MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
 			}
 			measuredHeight += mHeader.getMeasuredHeight();
-		} else if (mDivider != null&&mItem.getVisibility()!=View.GONE) {
+		} else if (mDivider != null && mItem != null && mItem.getVisibility()!=View.GONE) {
 			measuredHeight += mDividerHeight;
 		}
-		
+
 		//measure item
-		ViewGroup.LayoutParams params = mItem.getLayoutParams();
-        //enable hiding listview item,ex. toggle off items in group
-		if(mItem.getVisibility()==View.GONE){
-            mItem.measure(childWidthMeasureSpec,
-                    MeasureSpec.makeMeasureSpec(0, MeasureSpec.EXACTLY));
-        }else if (params != null && params.height >= 0) {
-			mItem.measure(childWidthMeasureSpec,
-					MeasureSpec.makeMeasureSpec(params.height, MeasureSpec.EXACTLY));
-            measuredHeight += mItem.getMeasuredHeight();
-		} else {
-			mItem.measure(childWidthMeasureSpec,
-					MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-            measuredHeight += mItem.getMeasuredHeight();
+		if (mItem != null) {
+    		ViewGroup.LayoutParams params = mItem.getLayoutParams();
+            //enable hiding listview item,ex. toggle off items in group
+    		if(mItem.getVisibility()==View.GONE){
+                mItem.measure(childWidthMeasureSpec,
+                        MeasureSpec.makeMeasureSpec(0, MeasureSpec.EXACTLY));
+            }else if (params != null && params.height >= 0) {
+    			mItem.measure(childWidthMeasureSpec,
+    					MeasureSpec.makeMeasureSpec(params.height, MeasureSpec.EXACTLY));
+                measuredHeight += mItem.getMeasuredHeight();
+    		} else {
+    			mItem.measure(childWidthMeasureSpec,
+    					MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+                measuredHeight += mItem.getMeasuredHeight();
+    		}
+
 		}
-
-
 		setMeasuredDimension(measuredWidth, measuredHeight);
 	}
 
@@ -144,21 +140,21 @@ public class WrapperView extends ViewGroup {
 			int headerHeight = mHeader.getMeasuredHeight();
 			mHeader.layout(l, t, r, headerHeight);
 			mItemTop = headerHeight;
-			mItem.layout(l, headerHeight, r, b);
 		} else if (mDivider != null) {
 			mDivider.setBounds(l, t, r, mDividerHeight);
 			mItemTop = mDividerHeight;
-			mItem.layout(l, mDividerHeight, r, b);
 		} else {
 			mItemTop = t;
-			mItem.layout(l, t, r, b);
+		}
+		if (mItem != null) {
+		    mItem.layout(l, mItemTop, r, b);
 		}
 	}
 
 	@Override
 	protected void dispatchDraw(Canvas canvas) {
 		super.dispatchDraw(canvas);
-		if (mHeader == null && mDivider != null&&mItem.getVisibility()!=View.GONE) {
+		if (mHeader == null && mDivider != null && mItem != null && mItem.getVisibility()!=View.GONE) {
 			// Drawable.setBounds() does not seem to work pre-honeycomb. So have
 			// to do this instead
 			if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
