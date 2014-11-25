0dbfd08cf8c350315efef824d6b2c6a88441d478
Ben Neill <b.neill@bookmaker.com.au>
2014-11-25 11:47:12 +1000

    [#354] Added xml attribute to set headers as collapsed by default. Also added to sample.

--- library/src/se/emilsjolander/stickylistheaders/ExpandableStickyListHeadersListView.java
+++ library/src/se/emilsjolander/stickylistheaders/ExpandableStickyListHeadersListView.java
@@ -1,6 +1,7 @@
 package se.emilsjolander.stickylistheaders;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -8,26 +9,27 @@ import java.util.List;
 
 /**
  * add expand/collapse functions like ExpandableListView
+ *
  * @author lsjwzh
  */
 public class ExpandableStickyListHeadersListView extends StickyListHeadersListView {
-    public interface IAnimationExecutor{
-        public void executeAnim(View target,int animType);
+    public interface IAnimationExecutor {
+        public void executeAnim(View target, int animType);
     }
 
     public final static int ANIMATION_COLLAPSE = 1;
     public final static int ANIMATION_EXPAND = 0;
 
     ExpandableStickyListHeadersAdapter mExpandableStickyListHeadersAdapter;
-
+    private boolean mStartCollapsed;
 
 
     IAnimationExecutor mDefaultAnimExecutor = new IAnimationExecutor() {
         @Override
         public void executeAnim(View target, int animType) {
-            if(animType==ANIMATION_EXPAND){
+            if (animType == ANIMATION_EXPAND) {
                 target.setVisibility(VISIBLE);
-            }else if(animType==ANIMATION_COLLAPSE){
+            } else if (animType == ANIMATION_COLLAPSE) {
                 target.setVisibility(GONE);
             }
         }
@@ -40,10 +42,28 @@ public class ExpandableStickyListHeadersListView extends StickyListHeadersListVi
 
     public ExpandableStickyListHeadersListView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        if(attrs != null) {
+            TypedArray typedArray = context.obtainStyledAttributes(attrs,
+                    R.styleable.ExpandableStickyListHeadersListView);
+
+            mStartCollapsed = typedArray.getBoolean(
+                    R.styleable.ExpandableStickyListHeadersListView_startCollapsed,
+                    false);
+            typedArray.recycle();
+        }
     }
 
     public ExpandableStickyListHeadersListView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        if(attrs != null) {
+            TypedArray typedArray = context.obtainStyledAttributes(attrs,
+                    R.styleable.ExpandableStickyListHeadersListView);
+
+            mStartCollapsed = typedArray.getBoolean(
+                    R.styleable.ExpandableStickyListHeadersListView_startCollapsed,
+                    true);
+            typedArray.recycle();
+        }
     }
 
     @Override
@@ -53,26 +73,26 @@ public class ExpandableStickyListHeadersListView extends StickyListHeadersListVi
 
     @Override
     public void setAdapter(StickyListHeadersAdapter adapter) {
-        mExpandableStickyListHeadersAdapter = new ExpandableStickyListHeadersAdapter(adapter);
+        mExpandableStickyListHeadersAdapter = new ExpandableStickyListHeadersAdapter(adapter, mStartCollapsed);
         super.setAdapter(mExpandableStickyListHeadersAdapter);
     }
 
-    public View findViewByItemId(long itemId){
+    public View findViewByItemId(long itemId) {
         return mExpandableStickyListHeadersAdapter.findViewByItemId(itemId);
     }
 
-    public long findItemIdByView(View view){
+    public long findItemIdByView(View view) {
         return mExpandableStickyListHeadersAdapter.findItemIdByView(view);
     }
 
     public void expand(long headerId) {
-        if(!mExpandableStickyListHeadersAdapter.isHeaderCollapsed(headerId)){
+        if (!mExpandableStickyListHeadersAdapter.isHeaderCollapsed((int) headerId)) {
             return;
         }
-        mExpandableStickyListHeadersAdapter.expand(headerId);
+        mExpandableStickyListHeadersAdapter.expand((int) headerId);
         //find and expand views in group
         List<View> itemViews = mExpandableStickyListHeadersAdapter.getItemViewsByHeaderId(headerId);
-        if(itemViews==null){
+        if (itemViews == null) {
             return;
         }
         for (View view : itemViews) {
@@ -81,13 +101,13 @@ public class ExpandableStickyListHeadersListView extends StickyListHeadersListVi
     }
 
     public void collapse(long headerId) {
-        if(mExpandableStickyListHeadersAdapter.isHeaderCollapsed(headerId)){
+        if (mExpandableStickyListHeadersAdapter.isHeaderCollapsed((int) headerId)) {
             return;
         }
-        mExpandableStickyListHeadersAdapter.collapse(headerId);
+        mExpandableStickyListHeadersAdapter.collapse((int) headerId);
         //find and hide views with the same header
         List<View> itemViews = mExpandableStickyListHeadersAdapter.getItemViewsByHeaderId(headerId);
-        if(itemViews==null){
+        if (itemViews == null) {
             return;
         }
         for (View view : itemViews) {
@@ -95,8 +115,8 @@ public class ExpandableStickyListHeadersListView extends StickyListHeadersListVi
         }
     }
 
-    public boolean isHeaderCollapsed(long headerId){
-        return  mExpandableStickyListHeadersAdapter.isHeaderCollapsed(headerId);
+    public boolean isHeaderCollapsed(long headerId) {
+        return mExpandableStickyListHeadersAdapter.isHeaderCollapsed((int) headerId);
     }
 
     public void setAnimExecutor(IAnimationExecutor animExecutor) {
@@ -111,14 +131,14 @@ public class ExpandableStickyListHeadersListView extends StickyListHeadersListVi
      *               or ExpandCollapseAnimation.EXPAND
      */
     private void animateView(final View target, final int type) {
-        if(ANIMATION_EXPAND==type&&target.getVisibility()==VISIBLE){
+        if (ANIMATION_EXPAND == type && target.getVisibility() == VISIBLE) {
             return;
         }
-        if(ANIMATION_COLLAPSE==type&&target.getVisibility()!=VISIBLE){
+        if (ANIMATION_COLLAPSE == type && target.getVisibility() != VISIBLE) {
             return;
         }
-        if(mDefaultAnimExecutor !=null){
-            mDefaultAnimExecutor.executeAnim(target,type);
+        if (mDefaultAnimExecutor != null) {
+            mDefaultAnimExecutor.executeAnim(target, type);
         }
 
     }
